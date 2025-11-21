ec5784f36d9aa461f9758a58795f96819bacbf59
Md Inzamamul Lohani <hamimlohani@gmail.com>
2025-11-21 18:32:56 +0600

    version v1.0.4

--- system_suite.sh
+++ system_suite.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 #############################
 # Global Configuration
 #############################
-SCRIPT_VERSION="1.0.1"
+SCRIPT_VERSION="1.0.4"
 SCRIPT_NAME="System Suite"
 CONFIG_DIR="${HOME}/.config/system_suite"
 DATA_DIR="${HOME}/.local/share/system_suite"
